6969c1dbf08a65343d3b328887a30157960fa0fc
Rafaello Bertolini <rafaellobertolini@gmail.com>
2019-10-10 01:28:29 +0100

    Adding sites to skip

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     chrome.tabs.executeScript(null, { code:
-        'var elem = document.getElementById("5oLVMexlQNYUMMmO");' +
-        'if (elem === null) {' +
-            'var style = document.createElement("style");' +
-            'style.setAttribute("id", "5oLVMexlQNYUMMmO");' +
-            'style.setAttribute("color", "black");' +
-            'style.innerHTML = "123";' +
-            'document.documentElement.appendChild(style);' +
+        'var url = window.location.href;' +
+        'var sitesToSkip = ["youtube", "kissanime", "animeflix"];' +
+        'var regex = new RegExp(sitesToSkip.join("|"), "i");' +
+        'var skipSite = regex.test(url);' +
+        'if (!skipSite) {' +
+            'var elem = document.getElementById("5oLVMexlQNYUMMmO");' +
+            'if (elem === null) {' +
+                'var style = document.createElement("style");' +
+                'style.setAttribute("id", "5oLVMexlQNYUMMmO");' +
+                'style.setAttribute("color", "black");' +
+                'style.innerHTML = "123";' +
+                'document.documentElement.appendChild(style);' +
+            '}' +
         '}'
     });
 });
